44f791a89671f91d4daa99aed100260ff87e4c90
Zorous <oussamabelhadi5@gmail.com>
2023-02-08 23:53:57 +0100

    accessing test table

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,14 @@ app.get("/", (req,res)=>{
     res.json('hello this is the backend')
 })
 
+app.get("/books",(req,res)=>{
+    const q = "SELECT * FROM users";
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data)
+    });
+
+})
 
 app.listen(8800,()=>{
     console.log('connected to backend')
