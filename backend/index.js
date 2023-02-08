90d09f8fd09bda33fc0fc6a0b81fbd56dfd7f7ec
Zorous <oussamabelhadi5@gmail.com>
2023-02-08 23:47:37 +0100

    connecting to mysql via  mysql express module

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 import express from "express";
-
+import mysql from "mysql";
 
 
 const app = express();
 
+
+const db = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password:"",
+    database : "react-mysql demo"
+})
 
 
 app.listen(8800,()=>{
